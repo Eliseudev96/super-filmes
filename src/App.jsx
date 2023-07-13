5914950b99193155de01f95a664f76c769fefff2
Eliseudev96 <eliseuespada@gmail.com>
2023-07-13 18:18:05 -0300

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -19,14 +19,6 @@ const App = () => {
           src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7267895515560863'
           crossorigin='anonymous'
         ></script>
-        <script
-          type='text/javascript'
-          src='//pl20040876.highwaycpmrevenue.com/73/28/11/732811c6c6afcb41c45722b48bb407d8.js'
-        ></script>
-        <script
-          type='text/javascript'
-          src='//pl20040936.highwaycpmrevenue.com/36/4a/7d/364a7dcd2cfac2e791b464b20cd412fd.js'
-        ></script>
         <Analytics />
         <MovieList />
       </div>
